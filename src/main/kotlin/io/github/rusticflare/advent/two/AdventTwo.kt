faa3cc052b414496827c8be77dc63aef01ec2b93
James Baker <j_d_baker@hotmail.com>
2018-12-06 19:12:43 +0000

    Complete Day 2 Star 2

--- src/main/kotlin/io/github/rusticflare/advent/two/AdventTwo.kt
+++ src/main/kotlin/io/github/rusticflare/advent/two/AdventTwo.kt
@@ -2,6 +2,7 @@ package io.github.rusticflare.advent.two
 
 import java.nio.file.Files
 import java.nio.file.Paths
+import java.util.*
 import java.util.stream.Stream
 
 class AdventTwo {
@@ -14,14 +15,21 @@ class AdventTwo {
     }
 
     fun second() {
-        println("Second: ")
+        val match = boxIds()
+            .flatMap { main -> boxIds().map { main.getSimilar(it) }.flatMap { it.stream() } }
+            .findFirst()
+            .orElseThrow()
+        println("Second: $match")
     }
 
     private fun boxIds(): Stream<BoxId> {
+        return streamLines()
+            .map { BoxId(it) }
+    }
+
+    private fun streamLines(): Stream<String> {
         val inputPath = Paths.get(this::class.java.getResource("/two/input.txt").toURI())
         return Files.newBufferedReader(inputPath).lines()
-            .map { BoxId(it) }
-
     }
 
 }
--- src/main/kotlin/io/github/rusticflare/advent/two/BoxId.kt
+++ src/main/kotlin/io/github/rusticflare/advent/two/BoxId.kt
@@ -1,5 +1,6 @@
 package io.github.rusticflare.advent.two
 
+import java.util.*
 import java.util.stream.Collectors.counting
 import java.util.stream.Collectors.groupingBy
 import kotlin.streams.asStream
@@ -16,6 +17,39 @@ class BoxId(private val id: String) {
         return id.asSequence().asStream()
             .collect(groupingBy({ it }, counting()))
             .containsValue(3);
+    }
+
+    fun getSimilar(other: BoxId): Optional<String> {
+        if (this.id.length != other.id.length) {
+            return Optional.empty()
+        }
+        var misses = 0
+        var missAt = -1
+        for (i in 0 until this.id.length) {
+            if (this.id[i] != other.id[i]) {
+                misses++
+                missAt = i
+            }
+        }
+        if (misses == 1) {
+            return Optional.of(this.id.removeRange(IntRange(missAt, missAt)))
+        }
+        return Optional.empty()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as BoxId
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
     }
 
 }
